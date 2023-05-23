e73d16f53b0c8798445ecb53b9ee6bb50a42080a
Matheus4136572 <93297774+Matheus4136572@users.noreply.github.com>
2023-05-22 21:02:11 -0300

    Add files via upload

---
+++ api/index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>GABRIEL</title>
+    <style>
+        body {
+            display: block;
+            background-size: cover;
+            background-position: relative;
+            background-image: url(Music.png);
+            background-repeat: no-repeat;
+        }
+        .tela {
+            display: flex;
+            position: absolute;
+            top: 50%;
+            left: 50%;
+            transform: translate(-50%, -50%);
+        }
+    </style>
+</head>
+<body>
+    <div class="tela">
+        <?php
+            $g = "GABRIEL";
+            $c = "JUCATI"; 
+            $d = "DE";
+            echo $g . " " . $d . " " . $c;
+        ?>
+    </div>
+</body>
+</html>
